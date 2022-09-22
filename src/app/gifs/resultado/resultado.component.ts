57e40c49370918f6e47409b5a7bf0d773a7643db
marleneherke <marlene.herke@live.de>
2022-09-22 12:33:24 +0200

    Gifs-App

---
+++ src/app/gifs/resultado/resultado.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
+
+@Component({
+  selector: 'app-resultado',
+  templateUrl: './resultado.component.html',
+  styles: [
+  ]
+})
+export class ResultadoComponent {
+
+  get resultados() {
+    return this.gifsService.resultados;
+  }
+
+  constructor(private gifsService: GifsService) { }
+
+
+}
